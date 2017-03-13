0404a380a3c1ba56c546e384f029ad16d637928f
Jan Schwarz <jan.schwarz@strv.com>
2017-03-13 10:08:27 +0100

    Initial Xcode project setup

---
+++ RapidSDK/RapidSDK/RapidSDK.h
@@ -0,0 +1,19 @@
+//
+//  RapidSDK.h
+//  RapidSDK
+//
+//  Created by Jan on 13/03/2017.
+//  Copyright © 2017 Rapid.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RapidSDK.
+FOUNDATION_EXPORT double RapidSDKVersionNumber;
+
+//! Project version string for RapidSDK.
+FOUNDATION_EXPORT const unsigned char RapidSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RapidSDK/PublicHeader.h>
+
+
