04c8bf59a3e6573865c51ff2c1a1ee8df0ae0c1e
Jan Schwarz <jan.schwarz@strv.com>
2017-03-14 10:40:41 +0100

    Build universal framework Initialize example project

---
+++ RapidSDK/RapidSDK/Rapid.swift
@@ -0,0 +1,16 @@
+//
+//  Rapid.swift
+//  RapidSDK
+//
+//  Created by Jan on 13/03/2017.
+//  Copyright © 2017 Rapid.io. All rights reserved.
+//
+
+import Foundation
+
+open class Rapid {
+    
+    open class func initialize() {
+        print("prdel")
+    }
+}
