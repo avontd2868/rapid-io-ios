7b9dbc5fadb222764adaca8c3f87c589eef0735a
Jan Schwarz <jan.schwarz@strv.com>
2017-03-15 16:11:06 +0100

    Initialize for Carthage

---
+++ Example/ExampleCarthageApp/Carthage/Checkouts/ios/Rapid/Rapid/Rapid.swift
@@ -0,0 +1,16 @@
+//
+//  Rapid.swift
+//  Rapid
+//
+//  Created by Jan on 14/03/2017.
+//  Copyright © 2017 Rapid.io. All rights reserved.
+//
+
+import Foundation
+
+open class Rapid {
+    
+    open class func configure(withAPIKey key: String) {
+        
+    }
+}
