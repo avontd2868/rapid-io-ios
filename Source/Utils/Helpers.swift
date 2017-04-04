8ed68bc3bc928638db015ceb4c9fab0427903158
Jan Schwarz <jan.schwarz@strv.com>
2017-04-04 17:32:09 +0200

    Generate shorter identifiers

--- Source/Utils/Helpers.swift
+++ Source/Utils/Helpers.swift
@@ -22,8 +22,63 @@ class Generator {
     
     /// Unique ID which can serve as a document or collection ID
     class var uniqueID: String {
-        return NSUUID().uuidString
+        let shortID = base64(fromGuid: NSUUID())
+        return shortID
     }
+    
+    /// Array of 64 characters for GUID representation
+    static let byteCharArray = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-".characters)
+    
+    /// Get string representation of GUID
+    ///
+    /// Standard GUID string representation uses only hexadecimal characters (0123456789ABCDEF).
+    /// This method uses 64 characters so the 128-bit GUID can be encoded to 22 characters in comparison with 32 characters of standard GUID representation.
+    ///
+    /// - Parameter guid: `NSUUID` instance
+    /// - Returns: GUID string representation
+    class func base64(fromGuid guid: NSUUID) -> String {
+        // Get binary representation of GUID (it has 16 bytes)
+        var bytes = [UInt8](repeating: 0, count: 16)
+        guid.getBytes(&bytes)
+        let data = Data(bytes: bytes)
+        
+        var numberOfBitsCarried: UInt8 = 0
+        var carry: UInt8 = 0
+        var resultString = ""
+        
+        for byte in data.enumerated() {
+            numberOfBitsCarried += 2
+            
+            // Get 64-based number
+            // Take first `8 - numberOfBitsCarried` bits from the byte and prepend it with bits carried from last iteration
+            let shifted = (byte.element >> numberOfBitsCarried) + (carry << (8 - numberOfBitsCarried))
+            
+            // Append character to string
+            resultString += String(byteCharArray[Int(shifted)])
+            
+            // Carry those bits which were not used for the 64-based number computation
+            // It is last `numberOfBitsCarried` bits from the byte
+            carry = byte.element & (255 >> (8 - numberOfBitsCarried))
+            
+            // 6 bits are enough to get next 64-based number.
+            // So if 6 bits should be carried to a next iteration get 64-based number from it and do not carry anything
+            if numberOfBitsCarried == 6 {
+                resultString += String(byteCharArray[Int(carry)])
+                
+                carry = 0
+                numberOfBitsCarried = 0
+            }
+        }
+        
+        // Deal with last two bits
+        if numberOfBitsCarried > 0 {
+            let shifted = carry << (6 - numberOfBitsCarried)
+            resultString += String(byteCharArray[Int(shifted)])
+        }
+        
+        return resultString
+    }
+
 }
 
 class Decoder {
